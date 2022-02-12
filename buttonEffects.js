13da79c6df512b8ad8a5d42c31e0f4103482c981
Jordan Wheeler <j.wheeler5291@gmail.com>
2022-02-12 17:38:53 -0600

    Add Script for dynamic buttons and favicon

---
+++ buttonEffects.js
@@ -0,0 +1,19 @@
+$(document).ready( function(){
+    // Listen to all the buttons for the press event
+    $(".button-container > div").not(".button-all").click(function() {
+        $(this).toggleClass("pressed");
+    });
+
+    //Use the "All" button to toggle everything
+    $(".button-all").click(function()
+    {
+        if($(this).hasClass("pressed"))
+        {
+            $(".button-container > div").removeClass("pressed");
+        }
+        else
+        {
+            $(".button-container > div").addClass("pressed");
+        }
+    });
+});
