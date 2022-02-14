5ab52a9218307cdbe250f12c21663298083ba9af
Jordan Wheeler <j.wheeler5291@gmail.com>
2022-02-14 14:39:26 -0600

    Try safe-area-inset-bottom

--- buttonEffects.js
+++ buttonEffects.js
@@ -1,10 +1,10 @@
 $(document).ready( function(){
 
     //Hacky nonsense to work around the "browser chrome"
-    $(window).resize(function() {
-        $("body").height(window.innerHeight*0.98);
-        console.log(window.innerHeight);
-    });
+    // $(window).resize(function() {
+    //     $("body").height(window.innerHeight*0.98);
+    //     // console.log(document.documentElement.clientHeight);
+    // });
 
     // Listen to all the buttons for the press event
     $(".button-container > div").not(".button-all").click(function() {
