3a22c829753f7a39a099fcfbb479d1a6cb17bb25
Jordan Wheeler <j.wheeler5291@gmail.com>
2022-02-14 11:40:27 -0600

    Change body innerHeight to center col

--- buttonEffects.js
+++ buttonEffects.js
@@ -1,6 +1,6 @@
 $(document).ready( function(){
     //Set the height to the inner height
-    $("body").height(window.innerHeight);
+    on$(".center-col").height(window.innerHeight * 0.98);
 
     // Listen to all the buttons for the press event
     $(".button-container > div").not(".button-all").click(function() {
